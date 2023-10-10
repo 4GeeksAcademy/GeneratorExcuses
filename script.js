6ce47036487669c2dacd7b6c33b23451b6c78f96
MarcelaZenteno93 <126695026+MarcelaZenteno93@users.noreply.github.com>
2023-10-10 20:19:21 +0000

    terminando , pero hay un error que corregir

--- script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    const who =['The dog', 'My grandma', 'His turtle' , 'My bird'];
-    const action =['ate', 'peed','crushed','broke'];
-    const what =[]
-}
--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+function generarExcusaAleatoria() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", " the keys", "the car"];
+  const when = [
+    "before the class ",
+    " right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  //Generador de indices aleatorios
+  const indiceWho = Math.floor(Math.random() * who.lengrh);
+  const indiceAction = Math.floor(Math.random() * action.length);
+  const indiceWhat = Math.floor(Math.random() * when.length);
+  const indiceWhen = Math.floor(Math.random() * when.length);
+
+  //Constructor de excusa aleatori
+  const excusa = `${who[indiceWho]} ${action[indiceAction]} ${what[indiceWhat]} ${when[indiceWhen]}`;
+
+  //Establece la excusa en el innerHTML del elemento con id "excuse"
+  document.getElementById("excuse").innerHTML = excusa;
+}
+document
+  .getElementById("generarButton")
+  .addEventListener("click", generarExcusaAleatoria);
